3c4db488ea6e10bd7cd2316d5b099eb6de0ed138
Juan David Castro Gallego <fjuandc@gmail.com>
2025-01-25 10:35:24 -0500

    feat: slow resize working

--- src/containers/EditingText.tsx
+++ src/containers/EditingText.tsx
@@ -1,4 +1,10 @@
-import { useCallback, type FocusEvent, type FC } from "react";
+import {
+  type FC,
+  type FocusEvent,
+  MouseEventHandler,
+  useCallback,
+  useState,
+} from "react";
 import { useDrag } from "react-dnd";
 import sanitizeHtml from 'sanitize-html';
 import type { IFixedElementEditingText } from "@/types";
@@ -13,26 +19,49 @@ export const EditingText: FC<Props> = ({
   ...props
 }) => {
   const { data: { x, y, text } } = props;
-  const [{ isDragging }, drag] = useDrag(
+  const [{ isDragging }, dragRef] = useDrag(
     () => ({
       type: dndTypes.EDITING_TEXT,
       item: { ...props },
       collect: (monitor) => ({
         isDragging: monitor.isDragging(),
       }),
-      isDragging() {
-        console.log("Dragging editting text");
-        return true;
-      },
-      // end: (_, monitor) => {
-      //   const didDrop = monitor.didDrop();
-      //   if (!didDrop) {
-      //     // open configbar
-      //   }
-      // },
     }),
     [props],
   );
+
+  const [drag, setDrag] = useState({ active: false, x: "", y: "" });
+
+  const [dims, setDims] = useState({ w: 200, h: 200 });
+
+  const boxStyle = { width: `${dims.w}px`, height: `${dims.h}px` };
+
+  const startResize: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDrag({
+      active: true,
+      x: `${e.clientX}`,
+      y: `${e.clientY}`,
+    });
+  };
+
+  const resizeFrame: MouseEventHandler<HTMLDivElement> = (e) => {
+    const { active, x, y } = drag;
+    if (active) {
+      const xDiff = Math.abs(+x - e.clientX);
+      const yDiff = Math.abs(+y - e.clientY);
+      const newW = +x > e.clientX ? dims.w - xDiff : dims.w + xDiff;
+      const newH = +y > e.clientY ? dims.h - yDiff : dims.h + yDiff;
+
+      setDrag({ ...drag, x: `${e.clientX}`, y: `${e.clientY}` });
+      setDims({ w: newW, h: newH });
+    }
+  };
+
+  const stopResize: MouseEventHandler<HTMLDivElement> = () => {
+    setDrag({ ...drag, active: false });
+  };
 
   const onContentBlur = useCallback(
     (evt: FocusEvent) => {
@@ -55,24 +84,43 @@ export const EditingText: FC<Props> = ({
 
   return (
     <div
-      ref={drag}
+      ref={dragRef}
       style={{
         opacity: isDragging ? 0.5 : 1,
-        width: "fit-content",
         position: "absolute",
         padding: ".5rem",
         border: "1px solid rgba(255,255,255,.1)",
         cursor: "move",
         top: y,
         left: x,
+        width: "fit-content",
+        height: "fit-content",
       }}
+      onMouseMove={resizeFrame} // TODO: this event should be in mobile frame, not text container
+      onMouseUp={stopResize}
     >
       <div
         onBlur={onContentBlur}
         contentEditable
         dangerouslySetInnerHTML={{ __html: `${html}` }}
-        style={{ cursor: "text" }}
+        style={{ cursor: "text", ...boxStyle }}
       />
+      <button
+        className="dragger"
+        onMouseDown={startResize}
+        style={{
+          backgroundColor: "rgba(255,255,255,.1)",
+          borderRadius: 0,
+          height: "1rem",
+          width: "1rem",
+          padding: 0,
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+          cursor: "se-resize",
+        }}
+      >
+      </button>
     </div>
   );
 };
