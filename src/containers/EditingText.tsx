2a6cf86c27d3b6b0f47e546718e51f81822e1b5b
Juan David Castro Gallego <fjuandc@gmail.com>
2025-01-24 22:02:52 -0500

    feat: contenteditable editing text component

--- src/containers/EditingText.tsx
+++ src/containers/EditingText.tsx
@@ -1,7 +1,10 @@
+import { useCallback, useState, type FocusEvent } from "react";
 import { useDrag } from "react-dnd";
+import sanitizeHtml from 'sanitize-html';
 import { dndTypes } from "@/constants/dnd";
 
 export const EditingText = () => {
+  const [value, setValue] = useState<string[]>(["EditingText"]);
   const [{ isDragging }, drag] = useDrag(
     () => ({
       type: dndTypes.EDITING_TEXT,
@@ -24,15 +27,38 @@ export const EditingText = () => {
     [],
   );
 
+  const onContentBlur = useCallback(
+    (evt: FocusEvent) => {
+      const sanitizeOpts = { allowedTags: [], allowedAttributes: {} };
+      const sanitize = (v: string) => sanitizeHtml(v, sanitizeOpts);
+      const newHtml = evt.currentTarget.innerHTML;
+      const newValue = `${newHtml}`
+        .replaceAll('</p><p>', '<br>')
+        .replaceAll('<p>', '')
+        .replaceAll('</p>', '')
+        .split('<br>')
+        .map(sanitize)
+        .filter((v) => v.length);
+      setValue(newValue);
+    },
+    []
+  );
+
+  const html = value.map(v => `<p>${v}</p>`).join("");
+
   return (
-    <div ref={drag} style={{
-      opacity: isDragging ? 0.5 : 1,
-      width: "fit-content",
-      position: "absolute",
-      top: 0,
-      left: 0,
-    }}>
-      Edit me (text)
-    </div>
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        width: "fit-content",
+        position: "absolute",
+        top: 0,
+        left: 0,
+      }}
+      onBlur={onContentBlur}
+      contentEditable
+      dangerouslySetInnerHTML={{ __html: `${html}` }}
+    />
   );
 };
